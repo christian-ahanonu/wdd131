ff81b80e09f3fd79f8d17fc913c8eb89148326d8
christian-ahanonu <ahanonuchristian116@gmail.com>
2024-05-19 20:10:53 +0100

    Light House and CSS Overview corrections.

--- week03/scripts/place.js
+++ week03/scripts/place.js
@@ -13,7 +13,24 @@ let displayContent = document.getElementById('lastmodified');
 displayContent.textContent = `Last Modification: ${dateLastModified}`;
 
 
+
+
 /*Calculate Wind Chill*/
 
-const windChill = document.querySelector('windchill');
+function calculateWindChill(temperature, windSpeed) {
+    if (temperature <= 10 && windSpeed > 4.8) {
+        // Wind chill calculation formula
+        const windChill = 13.12 + 0.6215 * temperature - 11.37 * Math.pow(windSpeed, 0.16) + 0.3965 * temperature * Math.pow(windSpeed, 0.16);
+        return windChill.toFixed(2); // Returning the wind chill rounded to two decimal places
+    } else {
+        return "N/A";
+    }
+}
 
+// Print wind chill value when the window loads
+window.onload = function() {
+    const temperature = 31;
+    const windSpeed = 5;
+    const windChillValue = calculateWindChill(temperature, windSpeed);
+    document.getElementById('windChill').textContent = `${windChillValue}`;
+};
